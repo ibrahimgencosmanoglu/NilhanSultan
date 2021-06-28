bf59d89ab387b796e3d78eea4d9e0c08612644ca
ibrahimgencosmanoglu <32437200+ibrahimgencosmanoglu@users.noreply.github.com>
2021-06-28 18:15:16 +0300

    ekleme

--- class/construct/switch-case.php
+++ class/construct/switch-case.php
@@ -43,6 +43,12 @@
 				_include_once('osmanli-donem-mutfaklari');
 		
 			break;
+			
+			case 'osmanli-donem-mutfaklari-devami';
+			
+				_include_once('osmanli-donem-mutfaklari-devami');
+		
+			break;
 
 			case 'osmanli-donem-mutfaklari-2';
 			
